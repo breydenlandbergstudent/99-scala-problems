a0754b5f35e9328b5fde5190f0110c0241248f01
Breyden Landberg <breyden.landberg@students.mq.edu.au>
2021-03-12 00:45:07 +1100

    finished P20

--- src/main/scala/P20.scala
+++ src/main/scala/P20.scala
@@ -10,25 +10,37 @@ import scala.annotation.tailrec
 // scala> removeAt(1, List('a, 'b, 'c, 'd))
 // res0: (List[Symbol], Symbol) = (List('a, 'c, 'd),'b)
 
-// not finished
-// TODO
 object P20 extends App {
-    // not finished
-    // TODO
     // uses an inner counter mechanism (not very idiomatic)
-    def remove (K : Int, list : List[Any]) : Option[(List[Any], Any)] = {
+    def removeOriginal (K : Int, list : List[Any]) : Option[(List[Any], Any)] = {
         @tailrec
         def innerRemove (removed : (List[Any], Any), innerK : Int, innerList : List[Any]) : Option[(List[Any], Any)] = {
             innerList match {
                 case Nil => Some (removed)
-                case hd :: tl if innerK > 0 => innerRemove ((removed._1 ::: List (hd), removed._2), innerK - 1, tl)
                 case hd :: tl if innerK == 0 => innerRemove ((removed._1, hd), innerK - 1, tl)
-                case hd :: tl if innerK < 0 => innerRemove ((removed._1 ::: List (hd), removed._2), innerK - 1, tl)
+                case hd :: tl => innerRemove ((removed._1 ::: List (hd), removed._2), innerK - 1, tl)
             }
         }
         innerRemove ((List (), Nil), K, list)
     }
 
+    // could use if/else?
+    def remove (K : Int, list : List[Any]) : Option[(List[Any], Any)] = {
+        @tailrec
+        def innerRemove (removed : (List[Any], Any), innerList : List[Any]) : Option[(List[Any], Any)] = {
+            innerList match {
+                case Nil => Some (removed)
+                case hd :: tl if list.length - innerList.length == K => innerRemove ((removed._1, hd), tl)
+                case hd :: tl => innerRemove ((removed._1 ::: List (hd), removed._2), tl)
+            }
+        }
+        innerRemove ((List (), Nil), list)
+    }
+
+    println ("P20 original solution given K = 3, list = List (0, 1, 2, 3, 4, 5)) is:   " + removeOriginal (3, List (0, 1, 2, 3, 4, 5)))
     println ("P20 solution given K = 3, list = List (0, 1, 2, 3, 4, 5)) is:   " + remove (3, List (0, 1, 2, 3, 4, 5)))
+    println ("P20 original solution given K = 7, list = List (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)) is:   " + removeOriginal (7, List (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)))
+    println ("P20 solution given K = 7, list = List (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)) is:   " + remove (7, List (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)))
+    println ("P20 original solution given K = 3, list = List ()) is:   " + removeOriginal (3, List ()))
     println ("P20 solution given K = 3, list = List ()) is:   " + remove (3, List ()))
 }
