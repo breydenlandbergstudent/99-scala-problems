4b4003e9f7789e71c575a23d12905fed634910cd
Breyden Landberg <breyden.landberg@students.mq.edu.au>
2021-03-11 17:33:45 +1100

    updated Main to handle P18

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -33,8 +33,8 @@ object Main extends App {
     println
     P17 main null
     println
-//    P18 main null
-//    println
+    P18 main null
+    println
 //    P19 main null
 //    println
 //    P20 main null
