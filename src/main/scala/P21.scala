a4502ec13f5a53e0cef650c029fe44324d1c2a4d
Breyden Landberg <breyden.landberg@students.mq.edu.au>
2021-03-12 16:02:18 +1100

    finished P21

--- src/main/scala/P21.scala
+++ src/main/scala/P21.scala
@@ -9,25 +9,20 @@ import scala.annotation.tailrec
 // scala> insertAt('new, 1, List('a, 'b, 'c, 'd))
 // res0: List[Symbol] = List('a, 'new, 'b, 'c, 'd)
 
-// not finished
-// TODO
 object P21 extends App {
-    // not finished
-    // TODO
-    // uses an inner counter mechanism (not very idiomatic)
-    def insert (item : Any, K : Int, list : List[Any]) : Option[List[Any]] = {
+    def insertAt (item : Any, K : Int, list : List[Any]) : Option[List[Any]] = {
         @tailrec
-        def innerInsert (runningList : List[Any], innerK : Int, innerList : List[Any]) : Option[List[Any]] = {
+        def innerInsert (runningList : List[Any], innerList : List[Any]) : Option[List[Any]] = {
             innerList match {
                 case Nil => Some (runningList)
-                case hd :: tl if innerK == 0 => innerInsert (runningList ::: List (item) ::: List (hd), innerK - 1, tl)
-                case hd :: tl if innerK > 0 => innerInsert (runningList ::: List (hd), innerK - 1, tl)
-                case hd :: tl if innerK < 0 => innerInsert (runningList ::: List (hd), innerK - 1, tl)
+                case hd :: tl if list.length - innerList.length == K => innerInsert (runningList ::: List (item) ::: List (hd), tl)
+                case hd :: tl => innerInsert (runningList ::: List (hd), tl)
             }
         }
-        innerInsert (List (), K, list)
+        innerInsert (List (), list)
     }
 
-    println ("P21 solution given item = \"inserted\", K = 4, list = List (0, 1, 2, 3, 4, 5)) is:   " + insert ("inserted", 4, List (0, 1, 2, 3, 4, 5)))
-    println ("P21 solution given item = \"inserted\", K = 4, list = List ()) is:   " + insert ("inserted", 4, List ()))
+    println ("P21 solution given item = \"inserted\", K = 4, list = List (0, 1, 2, 3, 4, 5)) is:   " + insertAt ("inserted", 4, List (0, 1, 2, 3, 4, 5)))
+    println ("P21 solution given item = \"inserted\", K = 7, list = List (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)) is:   " + insertAt ("inserted", 7, List (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)))
+    println ("P21 solution given item = \"inserted\", K = 4, list = List ()) is:   " + insertAt ("inserted", 4, List ()))
 }
